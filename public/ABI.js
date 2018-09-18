3aab84779584fecafcda081a476bd74c4f1caeb1
Glitch (diligent-maraca) <none>
2018-09-18 23:27:48 +0000

    📚💩 Updated with Glitch

---
+++ public/ABI.js
@@ -0,0 +1,10 @@
+import {
+  FIELD_TYPES,
+  canRenderMethodParams,
+  renderMethodParams,
+  canRenderMethodOutputs,
+  renderMethodOutputs
+} from 'ethereum-abi-ui'
+
+import web3 from "../ethereum/web3";
+
--- public/client.js
+++ public/client.js
@@ -1,45 +1,57 @@
-// client-side js
-// run by the browser each time your view template referencing it is loaded
-import {
-  FIELD_TYPES,
-  canRenderMethodParams,
-  renderMethodParams,
-  canRenderMethodOutputs,
-  renderMethodOutputs
-} from 'ethereum-abi-ui'
 
-console.log('hello world :o');
 
-// define variables that reference elements on our page
-const ABI = [ /* Solidity contract ABI definition */ ]
-const form = $('method')
-const output = $('outputs')
+const abiForm = document.forms[0];
+const abiInput = abiForm.elements['abi'];
 
-const fields = []
+// listen for the form to be submitted and set the new ABI
+abiForm.onsubmit = function(event) {
+  event.preventDefault();
+  console.log("ABI submitted");
+  document.getElementById("abi").innerHTML = abiInput.value;
+  
+  //do stuff using ethereum-abi-ui
+  
+  
+  // reset form 
+  abiInput.value = '';
+  abiInput.focus();
+};
 
-function submit() {
-  const values = {}
+// let ABI = [ ]
+  // {
 
-  fields.forEach(input => {
-    // sanitize entered value
-    const val = input.instance.sanitize(input.val())
 
-    // check that it's valid
-    if (!input.instance.isValid(val)) {
-      throw new Error('Please enter valid data')
-    }
 
-    // add to final values to send
-    values[input.getAttribute('name')] = val
-  })
+// function submit() {
+//   const values = {}
 
-  const results = doWeb3MethodCallUsingFormFieldValues(values)
+//   fields.forEach(input => {
+//     // sanitize entered value
+//     const val = input.instance.sanitize(input.val())
 
-  // now render the results
-  if (canRenderMethodOutputs(ABI, 'approve')) {
-    renderMethodOutputs(ABI, 'approve', results, (name, index, instance, result) => {
-      output.append(`<p>${name}: ${result}</p>`)
-    })
-  }
-}
+//     // check that it's valid
+//     if (!input.instance.isValid(val)) {
+//       throw new Error('Please enter valid data')
+//     }
+
+//     // add to final values to send
+//     values[input.getAttribute('name')] = val
+//   })
+
+//   		var contractspec = web3.eth.contract(ABI);
+//     contract = new web3.eth.Contract(JSON.parse(compiledGame.interface),contractAddress);
+  
+//   const results = contract.methods.register("Bob").send({
+//     value: web3.utils.toWei("1", "ether"),
+//     from: accounts[0],
+//     gas: 3000000
+//   });
+
+//   // now render the results
+//   if (canRenderMethodOutputs(ABI, 'approve')) {
+//     renderMethodOutputs(ABI, 'approve', results, (name, index, instance, result) => {
+//       output.append(`<p>${name}: ${result}</p>`)
+//     })
+//   }
+// }
 
